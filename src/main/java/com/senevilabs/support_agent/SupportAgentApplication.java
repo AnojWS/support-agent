b8432e612f5292f86da2c8ece77ba5c8f71d033f
AnojWS <anojws@gmail.com>
2026-08-16 23:18:03 +0530

    first commit

---
+++ src/main/java/com/senevilabs/support_agent/SupportAgentApplication.java
@@ -0,0 +1,13 @@
+package com.senevilabs.support_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senevilabs/support_agent/SupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senevilabs.support_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
